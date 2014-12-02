92ee46aca620d949bed53dc090984381caa1e6ad
Julia <juliazhang86@gmail.com>
2014-12-02 12:15:13 -0500

    landings show page

--- app/controllers/landings_controller.rb
+++ app/controllers/landings_controller.rb
@@ -7,9 +7,24 @@ class LandingsController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:user_id])
+    @landing = Landing.find(params[:id])
   end
 
   def create
+    @user = User.find(params[:user_id])
+    @landing = Landing.new(landing_params)
+    if @landing.save
+      redirect_to user_landing_path(@user, @landing)
+    else
+      render "new"
+    end
+  end
+
+  private
+
+  def landing_params
+    params.require(:landing).permit(:airport_id, :user_id, :destination_id, :ride_id, :arrival_time, :complete, :airline, :flight_num, :arrival_date)
   end
 
 end
