8c5abc17f6e2216e33fff55b8d0ed8723581d9a0
ayanthika <maryse686@yahoo.com>
2014-12-04 15:14:07 -0500

    updated landings show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to new_user_landing_path(@user)
+      redirect_to user_path(@user)
     else
       render "new"
     end
