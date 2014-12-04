e09fa71f9eca4ffa09b383c134a73250f3f4c2c0
ayanthika <maryse686@yahoo.com>
2014-12-04 13:54:37 -0500

    validations created

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -2,9 +2,13 @@ class DestinationsController < ApplicationController
 
   def create
     @user = User.find(params[:destination][:user_id])
-    @destination = Destination.create(destination_params)
-    @destination.get_neighborhood_and_borough
-    redirect_to user_path(@user)
+    @destination = Destination.new(destination_params)
+    if @destination.save
+      @destination.get_neighborhood_and_borough
+      redirect_to user_path(@user)
+    else
+      redirect_to user_path(@user), :notice => "error creating destination"
+    end
   end
 
   private
--- app/models/destination.rb
+++ app/models/destination.rb
@@ -1,8 +1,11 @@
 class Destination < ActiveRecord::Base
   has_many :landings
   belongs_to :user
+  validates :name, presence: true 
+  validate :validate_unique_user_destination
+  before_save :downcase_name
 
-  geocoded_by :address, {:latitude  => :lat, :longitude => :long, :neighborhood => "address.neighborhood"} # ActiveRecord
+  geocoded_by :address, {:latitude  => :lat, :longitude => :long} # ActiveRecord
   after_validation :geocode          # auto-fetch coordinates
 
   def get_neighborhood_and_borough
@@ -14,6 +17,18 @@ class Destination < ActiveRecord::Base
     self.update(:neighborhood => neighborhood, :borough => borough)
   end
 
+private
 
+  def validate_unique_user_destination
+    found_destination =  Destination.where(:user_id => self.user_id, :name => self.name.downcase)
+    # binding.pry
+    if found_destination.length > 0
+      errors.add(:name, "Must have unique name")
+    end
+  end
+
+  def downcase_name
+    self.name = self.name.downcase
+  end
 
 end
