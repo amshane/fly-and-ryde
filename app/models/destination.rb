14a7b5b60972042c9022954f3bff108e4271d087
sammarcus <sam.marcus@me.com>
2014-12-04 09:19:49 -0500

    added geo stuff

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -2,4 +2,8 @@ class Destination < ActiveRecord::Base
   has_many :landings
   belongs_to :user
 
+  geocoded_by :address, :latitude  => :lat, :longitude => :long # ActiveRecord
+  after_validation :geocode          # auto-fetch coordinates
+
+
 end
